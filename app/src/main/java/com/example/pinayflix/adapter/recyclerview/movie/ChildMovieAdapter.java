ad616ca373520810dce726c34ee3dc9b9e49511f
Matthew Nantes <matthewnantes33@gmail.com>
2021-06-02 22:25:47 +0800

    Code for review

--- app/src/main/java/com/example/pinayflix/adapter/recyclerview/movie/ChildMovieAdapter.java
+++ app/src/main/java/com/example/pinayflix/adapter/recyclerview/movie/ChildMovieAdapter.java
@@ -1,5 +1,6 @@
 package com.example.pinayflix.adapter.recyclerview.movie;
 
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,11 +10,16 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.pinayflix.R;
 import com.example.pinayflix.callback.OnMovieRequest;
 import com.example.pinayflix.databinding.ItemChildBinding;
@@ -57,7 +63,22 @@ public class ChildMovieAdapter extends RecyclerView.Adapter<ChildMovieAdapter.Ch
 
         Movie movie = data.get(position);
         ImageView target = holder.posterImageView;
-        requestManager.load(Uri.parse(Utils.POSTER_PATH + movie.getPosterPath()))
+
+        Log.d(TAG, "onBindViewHolder: Shimmer");
+
+        requestManager.
+                load(Uri.parse(Utils.POSTER_PATH + movie.getPosterPath())).addListener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                return true;
+            }
+        })
                 .apply(new RequestOptions().transform(new RoundedCorners(16)))
                 .into(target);
         holder.setOnClick(movie);
@@ -71,7 +92,6 @@ public class ChildMovieAdapter extends RecyclerView.Adapter<ChildMovieAdapter.Ch
 
     class ChildMovieViewHolder extends RecyclerView.ViewHolder {
         private ImageView posterImageView;
-
         public ChildMovieViewHolder(@NonNull ItemChildBinding binder) {
             super(binder.getRoot());
             posterImageView = binder.posterIv;
